c00ea1bc60e2d534e05967131cd5bf3d4a498f04
chalukapw4u <chalukawanniarachchi@gmail.com>
2025-12-09 05:31:33 +0530

    day3.1

---
+++ cat.py
@@ -0,0 +1,18 @@
+#using while
+i=0
+while i<=2:
+    print("meow")
+    i+=1
+
+#using for
+for i in [0,1,2]:
+    print("Meow")
+
+#modifying for
+for _ in range(9):
+    print("Meow")
+
+#another way
+print(("meow\n") * 3,end="")
+
+#getting a user input as a loop variable
